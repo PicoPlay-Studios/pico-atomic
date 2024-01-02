58207ace5e26cb1753534b7bd436d8b8448dc308
AmJoy01 <amber.lewis01@gmail.com>
2024-01-02 17:49:22 -0500

    user inputs matches element, then refreshes page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,20 +6,20 @@ import Keyboard from './components/Keyboard'
 import '@picocss/pico'
 import { useEffect, useState } from 'react'
 
-
-
-const rNum = Math.floor(Math.random() * 120);
-
+// const rNum = Math.floor(Math.random() * 120);
+// made it into a function
+// will rework this later so the character matches the number
+const generateRandomElement = () => Math.floor(Math.random() * 120)
 function App() {
 
     const [userInput, setUserInput] = useState('');
+	const [currentElement, setCurrentElement] = useState(generateRandomElement());
     //Insert key listener
     useEffect(() => {
         function handleInput(e){
             //handle delete
             if(e.key == 'Backspace'){
-                let s = userInput;
-                setUserInput(userInput => userInput.substring(0,s.length-1));
+                setUserInput((prevInput) => prevInput.slice(0,-1));
                 return;
             }
             //ignore inputs that are not keys 
@@ -30,8 +30,15 @@ function App() {
             if(userInput.length > 13){
                 return;
             }
-            setUserInput(userInput => userInput + e.key);
-            console.log(userInput);
+			const newUserInput = userInput + e.key
+            setUserInput(newUserInput);
+            // console.log(userInput);
+
+			//check if userInput matches the current element
+			if(parseInt(newUserInput, 10) === currentElement){
+				setCurrentElement(generateRandomElement)
+				setUserInput('')
+			}
         }
         window.addEventListener('keydown', handleInput);
 
@@ -39,16 +46,12 @@ function App() {
         return () => {
             window.removeEventListener('keydown', handleInput);
         }
-    },[userInput]);
-
-
-
-
+    },[userInput, currentElement]);
 
 	return (
 		<div className="margin-top" align="center">
 		<Title/>
-		<Element element={rNum}/>
+		<Element element={currentElement}/>
 		<Keyboard userInput={userInput}/>
 		</div>
 	)
